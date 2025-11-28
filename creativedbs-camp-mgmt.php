518c2523838d89cc9d6f8905b28be4512b06cb54
mkraner83 <info@creativedbs.com>
2025-11-28 11:36:05 +0000

    Release v2.3.3: Update login form styling with Annie Use Your Telescope font

--- creativedbs-camp-mgmt.php
+++ creativedbs-camp-mgmt.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: CreativeDBS Camp Management
 Description: Ultimate US Summer Camp Management Application.
-Version: 2.3.2
+Version: 2.3.3
 Author: CreativeDBS
 Text Domain: creativedbs-camp-mgmt
 */
 
-define('CDBS_CAMP_VERSION', '2.3.2');
+define('CDBS_CAMP_VERSION', '2.3.3');
 
 defined( 'ABSPATH' ) || exit;
 
